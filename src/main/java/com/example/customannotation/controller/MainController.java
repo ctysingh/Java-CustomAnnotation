c1008eb9563620a02b649fd20a443a7f63fb1e3d
ctysingh <chaitanyasingh@gmail.com>
2022-11-28 14:35:07 -0500

    adding a rest controller

---
+++ src/main/java/com/example/customannotation/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.example.customannotation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/test")
+    public List<Integer> testAnnotation(HttpServletRequest request) throws InterruptedException {
+        System.out.println("Request is authenticated");
+        List<Integer> ret = new ArrayList<>(Arrays.asList(100,2,10));
+        return ret;
+    }
+}
