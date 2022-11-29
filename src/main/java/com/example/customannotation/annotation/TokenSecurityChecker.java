3555ff91b226bdf230c57cb78053d890f91715c1
ctysingh <chaitanyasingh@gmail.com>
2022-11-28 20:12:05 -0500

    updated the logic in validator for access token

--- src/main/java/com/example/customannotation/annotation/TokenSecurityChecker.java
+++ src/main/java/com/example/customannotation/annotation/TokenSecurityChecker.java
@@ -8,13 +8,26 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Aspect
 @Component
 public class TokenSecurityChecker {
     @Around("@annotation(SecureWithToken)")
     public Object Validator(ProceedingJoinPoint call) throws Throwable, NotFoundException {
+
+        Object[] args = call.getArgs();
+        String access_token=null;
+        for(Object obj: args){
+            if(obj instanceof HttpServletRequest) {
+                access_token = ((HttpServletRequest) obj).getHeader("access_token"); //access_token supplied in header
+                if (access_token == null) {
+                    access_token = ((HttpServletRequest) obj).getParameter("access_token"); // access_roken supplied in request parameters    }
+                }
+            }
+        }
         Object result = call.proceed();
-        if(false) {
+        if(access_token != null && access_token.equals("validtoken") ) {
             return result;
         } else {
             throw new NotFoundException("Not Authorized");
